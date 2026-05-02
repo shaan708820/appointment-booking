ec39226677bfbf82af750ec56368f5974b2dc91f
Mohammad Shaan <mohdshaan708820@gmail.com>
2026-05-02 16:31:10 +0530

    Converted project to WAR file for Tomcat

--- src/main/java/com/example/appointment_booking/AppointmentBookingApplication.java
+++ src/main/java/com/example/appointment_booking/AppointmentBookingApplication.java
@@ -2,12 +2,19 @@ package com.example.appointment_booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppointmentBookingApplication {
+public class AppointmentBookingApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppointmentBookingApplication.class, args);
-	}
+    // This method is required for external Tomcat deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppointmentBookingApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentBookingApplication.class, args);
+    }
 }
